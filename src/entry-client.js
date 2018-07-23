eec07d1af1d8f6c532ab2634441f92fabffacf5a
Sun1ive <sunliveua@gmail.com>
2018-07-23 12:23:12 +0300

    'fix/router'

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,9 +1,8 @@
 import { createApp } from './main';
-import Vue from 'vue';
+// import Vue from 'vue';
+const Vue = require('vue').default;
 
 const { app, router, store } = createApp();
-
-console.log(router);
 
 if (window.__INITIAL_STATE__) {
   store.replaceState(window.__INITIAL_STATE__);
@@ -26,36 +25,5 @@ Vue.mixin({
 });
 
 router.onReady(() => {
-  console.log('123');
-  // Add router hook for handling asyncData.
-  // Doing it after initial route is resolved so that we don't double-fetch
-  // the data that we already have. Using router.beforeResolve() so that all
-  // async components are resolved.
-  router.beforeResolve((to, from, next) => {
-    const matched = router.getMatchedComponents(to);
-    const prevMatched = router.getMatchedComponents(from);
-    let diffed = false;
-    const activated = matched.filter((c, i) => {
-      return diffed || (diffed = prevMatched[i] !== c);
-    });
-    const asyncDataHooks = activated.map(c => c.asyncData).filter(_ => _);
-    if (!asyncDataHooks.length) {
-      return next();
-    }
-
-    bar.start();
-    Promise.all(asyncDataHooks.map(hook => hook({ store, route: to })))
-      .then(() => {
-        bar.finish();
-        next();
-      })
-      .catch(next);
-  });
-
-  // actually mount to DOM
-  // app.$mount('#app');
+  app.$mount('#app');
 });
-
-// router.onReady(() => {
-//   app.$mount('#app');
-// });
--- src/main.js
+++ src/main.js
@@ -5,17 +5,6 @@ import { createStore } from './store';
 import { createRouter } from './router';
 import './style.css';
 
-Vue.mixin({
-  beforeMount() {
-    const { asyncData } = this.$options;
-    if (asyncData) {
-      this.$data = asyncData({
-        store: this.$store,
-        route: this.$route,
-      });
-    }
-  },
-});
 
 export function createApp() {
   const router = createRouter();
@@ -24,7 +13,6 @@ export function createApp() {
   sync(store, router);
 
   const app = new Vue({
-    el: '#app',
     router,
     store,
     render: h => h(App),
